a9d794eb0a1878e7267bd8c7bb9ae0feabdb1d81
Andrea Ceballos <iraryc@gmail.com>
2024-05-02 09:35:28 -0700

    added names
    added names

--- demo.js
+++ demo.js
@@ -1,16 +1,26 @@
-function generateIcebreaker() {
+function generateNewName() {
   const questions = [
-    "What's your favorite way to unwind after a long day of coding?",
-    "What hobby would you get into if time and money weren't an issue?",
-    "What's the last book you read or movie you watched that you really enjoyed?",
-    "If you could visit any country in the world, where would you go and why?",
-    "What's one skill you'd like to develop or hobby you'd like to take up?",
-    "What's the most memorable vacation you've ever had?",
-    "If you could have dinner with any historical figure, who would it be and why?",
-    "What's your favorite season of the year and why?",
-    "What’s your favorite dish to cook or eat?",
-    "If you could instantly become an expert in something, what would it be?",
-  ];
+    "Hi, Sabrina",
+    "Hi, Monce",
+    "Hi, Donny",
+    "Hi, Alfi",
+    "Hi, Kayla",
+    "Hi, Kaaliyah",
+    "Hi, Brett",
+    "Hi, Jesus",
+    "Hi, Remsey",
+    "Hi, Abdi",
+    "Hi, Rene",
+    "Hi, Daniel",
+    "Hi, Kadi",
+    "Hi, Uriel",
+    "Hi, Victor",
+    "Hi, Guadalupe",
+    "Hi, Jeranesca",
+    "Hi, Leslie",
+    "Hi, Maximo",
+    "Hi, Niran",
+    ];
 
   // Randomly select a question
   const index = Math.floor(Math.random() * questions.length);
